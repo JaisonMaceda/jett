14354947c56e1fb8b56269ee7b0c9d02abacaf59
JaisonMaceda <43495431+JaisonMaceda@users.noreply.github.com>
2021-01-06 21:01:40 +0800

    add jett-ui remove jett

--- jett-ui/e2e/src/app.e2e-spec.ts
+++ jett-ui/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('jett app is running!');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    expect(await page.getTitleText()).toEqual('jett-ui app is running!');
   });
 
   afterEach(async () => {
