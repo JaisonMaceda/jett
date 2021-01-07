2ba06eb8f17d8e1116ef8cb5b99a33a0d0d6fb41
JaisonMaceda <43495431+JaisonMaceda@users.noreply.github.com>
2021-01-07 22:55:18 +0800

    add app routing module login component

--- jett-ui/src/app/app.module.ts
+++ jett-ui/src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RestapiService } from './restapi.service';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [RestapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
