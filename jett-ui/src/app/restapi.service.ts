117853f5abba99b45dacd548caafa0ff03544a76
JaisonMaceda <43495431+JaisonMaceda@users.noreply.github.com>
2021-01-07 23:19:46 +0800

    remove jett folder

--- jett-ui/src/app/restapi.service.ts
+++ jett-ui/src/app/restapi.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestapiService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public login(username:string, password:string){
+    const headers = new HttpHeaders({Authorization: 'Basic '+btoa(username+":"+password)})
+    this.http.get("http://localhost:8081/");
+  }
 }
