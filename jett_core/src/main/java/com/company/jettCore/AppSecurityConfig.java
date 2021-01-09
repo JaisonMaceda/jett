b564af34e7868ac2cd7e38b538da87a58b70ce64
JaisonMaceda <43495431+JaisonMaceda@users.noreply.github.com>
2021-01-09 19:24:20 +0800

    add configure method for login function

--- jett_core/src/main/java/com/company/jettCore/AppSecurityConfig.java
+++ jett_core/src/main/java/com/company/jettCore/AppSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,6 +20,12 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors();
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/**").fullyAuthenticated().and().httpBasic();
+	}
 	@Bean
 	public AuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
